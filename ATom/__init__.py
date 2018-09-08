34bd4ac38e09aca582cee5742b584e4607f3b26e
nhamilto <nicholas.hamilton@gmail.com>
2018-09-07 20:02:24 -0600

    updates

---
+++ ATom/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'Nicholas Hamilton'
+__email__ = 'nicholas.hamilton@nrel.gov'
+__version__ = 'v.1.0
+
+from ATom import atom_functions
+# from flowtools import snapshot_POD as POD
+# from flowtools import lidar_mod as lidar
+# from flowtools import flowvis as vis
+print('module initialized')
